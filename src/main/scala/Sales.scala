dbd7ed05878177229cd46ea8e8ba76d223f4f3c2
ThakurPriyanka <aroma.priyanka@gmail.com>
2018-02-26 10:53:05 +0530

    data is displaying

--- src/main/scala/Sales.scala
+++ src/main/scala/Sales.scala
@@ -1,13 +1,15 @@
 
+import org.apache.log4j.Logger
 import org.apache.spark.SparkContext
 import org.joda.time.DateTime
 class Sales {
 
   def getSales(sc: SparkContext): Unit = {
-    val file1 = sc.textFile("folder/folder3/file1")
+    val log = Logger.getLogger(this.getClass)
+    val file1 = sc.textFile("file1")
     val file1Data = file1.map(line => (line.split('#')(0),line.split('#').toList))
 
-    val file2 = sc.textFile("folder/folder3/file2")
+    val file2 = sc.textFile("file2")
     val file2Data = file2.map(line => (line.split('#')(1),line.split('#').toList))
 
     val joined = file1Data.join(file2Data)
@@ -17,22 +19,22 @@ class Sales {
 
     val finalYear = data.flatMap(x=> x._2.groupBy(y=>new DateTime((y._2._2(0)).toLong * 1000).toDateTime.toString("yyyy")))
 
-    val sumYear = finalYear.map(x=> (x._2.map(t=>t._2._1(4)),x._1,x._2.map(y=> y._2._2(2).toInt).sum))
+    val sumYear = finalYear.map(x=> (x._2.map(t=>t._2._1(4).distinct),x._1,x._2.map(y=> y._2._2(2).toInt).sum))
 
 
     val finalMonth = data.flatMap(x=> x._2.groupBy(y=>new DateTime((y._2._2(0)).toLong * 1000).toDateTime.toString("yyyy/MM")))
 
-    val sumMonth = finalMonth.map(x=> (x._2.map(t=>t._2._1(4)),x._1,x._2.map(y=> y._2._2(2).toInt).sum))
+    val sumMonth = finalMonth.map(x=> (x._2.map(t=>t._2._1(4).distinct),x._1,x._2.map(y=> y._2._2(2).toInt).sum))
 
     val finalDate = data.flatMap(x=> x._2.groupBy(y=>new DateTime((y._2._2(0)).toLong * 1000).toDateTime.toString("yyyy/MM/dd")))
 
-    val sumDate = finalDate.map(x=> (x._2.map(t=>t._2._1(4)),x._1,x._2.map(y=> y._2._2(2).toInt).sum))
+    val sumDate = finalDate.map(x=> (x._2.map(t=>t._2._1(4).distinct),x._1,x._2.map(y=> y._2._2(2).toInt).sum))
 
     val dataTemp = sumYear.union(sumMonth)
 
     val finalData = dataTemp.union(sumDate)
 
-    finalData.foreach(println)
+    log.info(s"\n\n\n\n\n\n\n\n\n${finalData.collect.toList}\n\n\n\n\n\n\n\n\n\n")
 
 
 
--- src/main/scala/SparkTest.scala
+++ src/main/scala/SparkTest.scala
@@ -2,9 +2,10 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkTest {
   def main(args: Array[String]): Unit = {
-    val sc = new SparkContext(new SparkConf().setAppName("spark-assignment1"))
+    val sc = new SparkContext(new SparkConf().setAppName("spark-assignment1").setMaster("local[*]"))
     val threshold = 3
     val objectSales = new Sales
     objectSales.getSales(sc)
+    Thread.sleep(10000)
   }
 }
